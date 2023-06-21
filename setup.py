e9985d03d8d628e08443bd84ea208def1fc45a6d
Seong-Hwan Jun <s2jun.uw@gmail.com>
2023-06-21 14:07:54 -0400

    separate library in setup.py

--- setup.py
+++ setup.py
@@ -5,6 +5,9 @@ setup(
     version="0.1",
     packages=find_packages(),
     install_requires=[
-        'torch', 'numpy', 'scipy', 'pandas'
+        'torch', 
+	'numpy', 
+	'scipy', 
+	'pandas'
     ],
 )
